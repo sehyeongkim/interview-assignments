bb88ff65d2f7833ffe6692a72cc9b28bf8183502
sehyeong <sshkim96@gmail.com>
2024-11-02 00:48:35 +0900

    test: initialize test settings

--- assignments/Back-End/Python/sehyeongkim/tests/conftest.py
+++ assignments/Back-End/Python/sehyeongkim/tests/conftest.py
@@ -1,6 +1,34 @@
+import pytest
+import asyncio
 import pytest_asyncio
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker, async_scoped_session
 
+from core.config import config
+from tests.test_db_handler import TestDBHandler
 
-@pytest_asyncio.fixture
-async def session():
-    pass
+test_db_handler = TestDBHandler()
+
+async_engine = create_async_engine(config.TEST_DB_URL)
+async_session_factory = async_sessionmaker(class_=AsyncSession, bind=async_engine, expire_on_commit=False)
+
+@pytest.fixture(scope='session', autouse=True)
+def initialize_database():
+    test_db_handler.apply_alembic()
+    yield
+
+@pytest.yield_fixture(scope='session')
+def event_loop(request):
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+@pytest_asyncio.fixture(scope='function', autouse=True)
+async def session(mocker):
+    """ Replace the testing db with the async session """
+    async_session = async_scoped_session(session_factory=async_session_factory, scopefunc=asyncio.current_task)
+    mocker.patch('core.db.transactional.session', async_session)
+    mocker.patch('app.user.services.session', async_session)
+    mocker.patch('app.post.services.session', async_session)
+    yield async_session
+    await async_session.remove()
+    test_db_handler.delete_all_rows()
---
+++ assignments/Back-End/Python/sehyeongkim/tests/test_db_handler.py
@@ -0,0 +1,39 @@
+from alembic import command
+from sqlalchemy import text, inspect, create_engine, Engine
+from alembic.config import Config as AlembicConfig
+
+from core.config import config
+
+
+class TestDBHandler:
+    __test__ = True
+
+    EXCLUDE_TABLES = {'alembic_version'}
+    TEST_DB_URL = config.TEST_DB_URL.replace('aiomysql', 'pymysql')
+
+    def apply_alembic(self) -> None:
+        alembic_cfg = AlembicConfig('alembic.ini')
+        alembic_cfg.set_main_option('is_testing', 'True')
+        alembic_cfg.set_main_option('sqlalchemy.url', config.TEST_DB_URL.replace('aiomysql', 'pymysql'))
+        command.upgrade(alembic_cfg, 'head')
+
+    def delete_all_rows(self) -> None:
+        engine = create_engine(url=self.TEST_DB_URL)
+        tables = self._get_all_tables(engine=engine)
+        for table in tables:
+            if table in self.EXCLUDE_TABLES:
+                continue
+            with engine.begin() as conn:
+                conn.execute(text(f'DELETE FROM {table}'))
+
+    def drop_all(self) -> None:
+        engine = create_engine(url=self.TEST_DB_URL)
+        tables = self._get_all_tables(engine=engine)
+        for table in tables:
+            with engine.begin() as conn:
+                conn.execute(text(f'DROP TABLE {table}'))
+
+    def _get_all_tables(self, engine: Engine) -> list[str]:
+        inspector = inspect(engine)
+        tables = [table_name for table_name in inspector.get_table_names()]
+        return tables
