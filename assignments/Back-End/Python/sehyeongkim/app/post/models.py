b74e575573de266e0972ec4db30e2c53e2da378d
sehyeong <sshkim96@gmail.com>
2024-11-03 02:37:10 +0900

    model: fix user_id column type

--- assignments/Back-End/Python/sehyeongkim/app/post/models.py
+++ assignments/Back-End/Python/sehyeongkim/app/post/models.py
@@ -1,4 +1,6 @@
+import uuid
 from sqlalchemy import Column, String, Integer, Text
+from sqlalchemy.dialects.mysql import BINARY
 
 from core.db.session import Base
 from core.db.mixins import TimestampMixin
@@ -8,6 +10,10 @@ class Post(Base, TimestampMixin):
     __tablename__ = 'posts'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(String(20), nullable=False)
+    user_id = Column(BINARY(16), nullable=False)
     title = Column(String(50), nullable=False)
     content = Column(Text, nullable=False)
+
+    @property
+    def user_id_str(self):
+        return str(uuid.UUID(bytes=self.user_id))
