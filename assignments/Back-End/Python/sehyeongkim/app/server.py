3c4afb1bb8537eaaa39197713694098e510424e0
sehyeong <sshkim96@gmail.com>
2024-11-02 09:50:36 +0900

    feat: add authentication backend in app middleware

--- assignments/Back-End/Python/sehyeongkim/app/server.py
+++ assignments/Back-End/Python/sehyeongkim/app/server.py
@@ -1,18 +1,32 @@
 from typing import List
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware import Middleware
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
 from api import router
 from core.config import config
+from core.exceptions.base import CustomException
 from core.exceptions.handler import init_exception_handler
 from core.fastapi.middlewares.sqlalchemy import SQLAlchemyMiddleware
+from core.fastapi.middlewares.authentication import AuthenticationMiddleware, AuthBackend
 
 
 def init_routers(app: FastAPI) -> None:
     app.include_router(router)
 
+def on_auth_error(request: Request, exc: Exception):
+    status_code, error_code, message = 401, None, str(exc)
+    if isinstance(exc, CustomException):
+        status_code = int(exc.code)
+        error_code = exc.error_code
+        message = exc.message
+
+    return JSONResponse(
+        status_code=status_code,
+        content={"error_code": error_code, "message": message},
+    )
 
 def make_middleware() -> List[Middleware]:
     middleware = [
@@ -22,6 +36,11 @@ def make_middleware() -> List[Middleware]:
             allow_credentials=True,
             allow_methods=["*"],
             allow_headers=["*"],
+        ),
+        Middleware(
+            AuthenticationMiddleware,
+            backend=AuthBackend(),
+            on_error=on_auth_error,
         ),
         Middleware(SQLAlchemyMiddleware)
     ]
