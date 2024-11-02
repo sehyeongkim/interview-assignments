7509e52dc1fecd50e529165a48205871b311df8e
sehyeong <sshkim96@gmail.com>
2024-11-03 02:39:50 +0900

    test: test get posts api test

--- assignments/Back-End/Python/sehyeongkim/tests/api/post/test_post.py
+++ assignments/Back-End/Python/sehyeongkim/tests/api/post/test_post.py
@@ -3,10 +3,23 @@ import pytest
 from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.server import app
 
 @pytest.mark.asyncio
-async def test_get_posts_list():
-    pass
+async def test_get_posts_list(create_users: dict):
+    headers = {'Authorization': f'Bearer {create_users["user_token"]}'}
+    async with AsyncClient(app=app, base_url='http://test') as client:
+        response = await client.get("/api/v1/posts", headers=headers)
+
+    assert response.status_code == 200
+
+@pytest.mark.asyncio
+async def test_get_posts_list_unauthorized(create_users: dict):
+    headers = {'Authorization': 'Bearer '}
+    async with AsyncClient(app=app, base_url='http://test') as client:
+        response = await client.get("/api/v1/posts", headers=headers)
+
+    assert response.status_code == 401
 
 @pytest.mark.asyncio
 async def test_create_post():
--- assignments/Back-End/Python/sehyeongkim/tests/app/post/test_services.py
+++ assignments/Back-End/Python/sehyeongkim/tests/app/post/test_services.py
@@ -1,15 +1,23 @@
 import pytest
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.post.models import Post
 from app.post.services import PostService
-
+from app.user.services import convert_uuid
 
 @pytest.mark.asyncio
 async def test_insert_post():
     pass
 
 @pytest.mark.asyncio
-async def test_get_posts_list():
-    pass
+async def test_get_posts_list(session: AsyncSession, create_users: dict):
+    post = Post(title='제목', content='내용', user_id=convert_uuid(create_users['user_id']))
+    session.add(post)
+    await session.commit()
+    await session.refresh(post)
+
+    result = await PostService().get_posts()
+    assert len(result) != 0
 
 @pytest.mark.asyncio
 async def test_get_post():
