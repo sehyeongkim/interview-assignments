c8866fea5b7e939d14e4f22daf6157c8e30a9724
sehyeong <sshkim96@gmail.com>
2024-11-03 11:36:05 +0900

    test: add permission test in post apis

--- assignments/Back-End/Python/sehyeongkim/tests/api/post/test_post.py
+++ assignments/Back-End/Python/sehyeongkim/tests/api/post/test_post.py
@@ -16,6 +16,14 @@ async def test_get_posts_list(init: dict):
     assert response.status_code == 200
 
 @pytest.mark.asyncio
+async def test_get_posts_list_unauthorized():
+    headers = {'Authorization': f'Bearer '}
+    async with AsyncClient(app=app, base_url='http://test') as client:
+        response = await client.get("/api/v1/posts", headers=headers)
+
+    assert response.status_code == 401
+
+@pytest.mark.asyncio
 async def test_create_post(init: dict):
     body = {
         'title': '제목',
@@ -39,6 +47,17 @@ async def test_create_post_invalid_request(init: dict):
     assert response.status_code == 422
 
 @pytest.mark.asyncio
+async def test_create_post_unauthorized():
+    body = {
+        'title': '제목',
+    }
+    headers = {'Authorization': f'Bearer '}
+    async with AsyncClient(app=app, base_url='http://test') as client:
+        response = await client.post("/api/v1/posts", headers=headers, json=body)
+
+    assert response.status_code == 401
+
+@pytest.mark.asyncio
 async def test_get_post(init: dict):
     headers = {'Authorization': f'Bearer {init["user_token"]}'}
     async with AsyncClient(app=app, base_url='http://test') as client:
@@ -47,11 +66,22 @@ async def test_get_post(init: dict):
     assert response.status_code == 200
 
 @pytest.mark.asyncio
+async def test_get_post_unauthorized(init: dict):
+    headers = {'Authorization': f'Bearer '}
+    async with AsyncClient(app=app, base_url='http://test') as client:
+        response = await client.get(f"/api/v1/posts/{init['post_id']}", headers=headers)
+
+    assert response.status_code == 401
+
+@pytest.mark.asyncio
 async def test_modify_post(session: AsyncSession, init: dict):
     new_content = '수정'
+    body = {
+        'content': new_content
+    }
     headers = {'Authorization': f'Bearer {init["user_token"]}'}
     async with AsyncClient(app=app, base_url='http://test') as client:
-        response = await client.put(f"/api/v1/posts/{init['post_id']}", headers=headers, json={'content': new_content})
+        response = await client.put(f"/api/v1/posts/{init['post_id']}", headers=headers, json=body)
 
     assert response.status_code == 200
     stmt = select(Post).where(Post.id==init['post_id'])
@@ -62,8 +92,23 @@ async def test_modify_post(session: AsyncSession, init: dict):
 
 @pytest.mark.asyncio
 async def test_modify_post_invalid_request(init: dict):
+    body = {
+        'title': 'not allowed'
+    }
     headers = {'Authorization': f'Bearer {init["user_token"]}'}
     async with AsyncClient(app=app, base_url='http://test') as client:
-        response = await client.put(f"/api/v1/posts/{init['post_id']}", headers=headers, json={'title': 'not allowed'})
+        response = await client.put(f"/api/v1/posts/{init['post_id']}", headers=headers, json=body)
 
     assert response.status_code == 422
+
+@pytest.mark.asyncio
+async def test_modify_post_forbidden(init: dict):
+    new_content = '수정'
+    body = {
+        'content': new_content
+    }
+    headers = {'Authorization': f'Bearer {init["superuser_token"]}'}
+    async with AsyncClient(app=app, base_url='http://test') as client:
+        response = await client.put(f"/api/v1/posts/{init['post_id']}", headers=headers, json=body)
+
+    assert response.status_code == 403
